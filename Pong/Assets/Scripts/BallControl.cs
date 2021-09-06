b49eb542a6c479613c8111e7ce4481b410f6da8a
DESKTOP-0283LTQ\darkc <dendi.stefanus@binus.ac.id>
2021-09-06 20:30:35 +0700

    Dendi Stefanus 2201744405

---
+++ Pong/Assets/Scripts/BallControl.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    
+    private Rigidbody2D rigidBody2D;
+    private Vector2 trajectoryOrigin;
+    
+    public float xInitialForce;
+    public float yInitialForce;
+
+
+    
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        trajectoryOrigin = transform.position;
+        RestartGame();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    void ResetBall()
+    {
+        
+        transform.position = Vector2.zero;
+ 
+        
+        rigidBody2D.velocity = Vector2.zero;
+    }
+    void PushBall()
+    {
+        
+        float yRandomInitialForce = yInitialForce;
+
+        
+        float randomDirection = Random.Range(0, 2);
+
+        
+        if (randomDirection < 1.0f)
+        {
+            
+            rigidBody2D.AddForce(new Vector2(-xInitialForce, yRandomInitialForce));
+        }
+        else
+        {
+            rigidBody2D.AddForce(new Vector2(xInitialForce, yRandomInitialForce));
+        }
+    }
+    public Vector2 TrajectoryOrigin
+    {
+        get { return trajectoryOrigin; }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        trajectoryOrigin = transform.position;
+    }
+    void RestartGame()
+    {
+        
+        ResetBall();
+ 
+        
+        Invoke("PushBall", 2);
+    }
+}
---
+++ Pong/Assets/Scripts/SideWalls.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWalls : MonoBehaviour
+{
+    [SerializeField]
+    private GameManager gameManager; 
+    public PlayerControls player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D anotherCollider)
+    {
+        
+        if (anotherCollider.name == "Ball")
+        {
+           
+            player.IncrementScore();
+ 
+            
+            if (player.Score < gameManager.maxScore)
+            {
+                
+                anotherCollider.gameObject.SendMessage("RestartGame", 2.0f, SendMessageOptions.RequireReceiver);
+            }
+        }
+    }
+}
